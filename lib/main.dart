040c0b1c44f517d6ba11d84a61fe332e31d59b22
ryhndastra <ryhndastra@gmail.com>
2025-07-14 20:14:28 +0700

    add theme and googlefonts dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,37 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.light(
+          primary: Colors.green,
+          secondary: Colors.greenAccent[400]!,
+        ),
+        textTheme: GoogleFonts.latoTextTheme(),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith<Color>(
+              (states) => Colors.green,
+            ),
+            shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
+              (states) => RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            padding: MaterialStateProperty.resolveWith<EdgeInsetsGeometry>(
+              (states) =>
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+            ),
+            textStyle: MaterialStateProperty.resolveWith<TextStyle>(
+              (states) => const TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          ),
         ),
       ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
