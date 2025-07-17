c2df7a3024af96137e29b5fb176770b705789502
ryhndastra <ryhndastra@gmail.com>
2025-07-17 17:59:18 +0700

    add navigator config

---
+++ lib/config/nav.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Nav {
+  static push(BuildContext context, Widget page) =>
+      Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+
+  static pushReplacement(BuildContext context, Widget page) =>
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => page),
+      );
+}
