0165148201108561f7782374cba513b112cd9b69
ryhndastra <ryhndastra@gmail.com>
2025-07-23 17:29:14 +0700

    change function name

--- lib/config/nav.dart
+++ lib/config/nav.dart
@@ -4,7 +4,7 @@ class Nav {
   static push(BuildContext context, Widget page) =>
       Navigator.push(context, MaterialPageRoute(builder: (context) => page));
 
-  static pushReplacement(BuildContext context, Widget page) =>
+  static replace(BuildContext context, Widget page) =>
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => page),
