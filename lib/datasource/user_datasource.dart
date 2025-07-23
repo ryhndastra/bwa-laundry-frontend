447a6ce0c6ccd45cc239615dd95c0a4a40cf72a1
ryhndastra <ryhndastra@gmail.com>
2025-07-23 17:29:40 +0700

    add login function

--- lib/datasource/user_datasource.dart
+++ lib/datasource/user_datasource.dart
@@ -6,6 +6,27 @@ import 'package:pasti_laundry/config/failure.dart';
 import 'package:http/http.dart' as http;
 
 class UserDatasource {
+  static Future<Either<Failure, Map>> login(
+    String email,
+    String password,
+  ) async {
+    Uri url = Uri.parse('${AppConstants.baseUrl}/login');
+    try {
+      final response = await http.post(
+        url,
+        headers: AppRequest.header(),
+        body: {'email': email, 'password': password},
+      );
+      final data = AppResponse.data(response);
+      return Right(data);
+    } catch (e) {
+      if (e is Failure) {
+        return Left(e);
+      }
+      return Left(FetchFailure(message: e.toString()));
+    }
+  }
+
   static Future<Either<Failure, Map>> register(
     String username,
     String email,
